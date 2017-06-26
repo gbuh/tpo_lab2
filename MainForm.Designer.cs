b0f677572097c1b08dc10ae6a10e616b159baf22
igor <igor@localhost>
2017-06-26 11:42:27 +0300

    v0.1

---
+++ MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace TestDriver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartRunEstimate = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonStartDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStartRunEstimate
+            // 
+            this.buttonStartRunEstimate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStartRunEstimate.Location = new System.Drawing.Point(157, 397);
+            this.buttonStartRunEstimate.Name = "buttonStartRunEstimate";
+            this.buttonStartRunEstimate.Size = new System.Drawing.Size(156, 23);
+            this.buttonStartRunEstimate.TabIndex = 0;
+            this.buttonStartRunEstimate.Text = "Тестирование RunEstimate";
+            this.buttonStartRunEstimate.UseVisualStyleBackColor = true;
+            this.buttonStartRunEstimate.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(546, 379);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonStartDel
+            // 
+            this.buttonStartDel.Location = new System.Drawing.Point(12, 397);
+            this.buttonStartDel.Name = "buttonStartDel";
+            this.buttonStartDel.Size = new System.Drawing.Size(139, 35);
+            this.buttonStartDel.TabIndex = 2;
+            this.buttonStartDel.Text = "Тестирование дел с ост";
+            this.buttonStartDel.UseVisualStyleBackColor = true;
+            this.buttonStartDel.Click += new System.EventHandler(this.buttonStartDel_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 432);
+            this.Controls.Add(this.buttonStartDel);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonStartRunEstimate);
+            this.MinimumSize = new System.Drawing.Size(578, 466);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Тестовый драйвер для калькулятора";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartRunEstimate;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonStartDel;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using BaseCalculator;
+
+namespace TestDriver
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // создаем провайдер для генерирования и компиляции кода на C#
+            System.CodeDom.Compiler.CodeDomProvider prov = System.CodeDom.Compiler.CodeDomProvider.CreateProvider("CSharp");
+            // создаем параметры компилирования
+            System.CodeDom.Compiler.CompilerParameters cmpparam = new System.CodeDom.Compiler.CompilerParameters();
+            // результат компиляции - библиотека
+            cmpparam.GenerateExecutable = false;
+            // не включаем информацию отладчика
+            cmpparam.IncludeDebugInformation = false;
+            // подключаем 2-е стандартные библиотеки и библиотеку CalcClass.dll
+            cmpparam.ReferencedAssemblies.Add(Application.StartupPath + "\\CalcClass.dll");
+            cmpparam.ReferencedAssemblies.Add("System.dll");
+            cmpparam.ReferencedAssemblies.Add("System.Windows.Forms.dll");
+            // имя выходной сборки - My.dll
+            cmpparam.OutputAssembly = "My.dll";
+            // компилируем класс AnalaizerClass с заданными параметрами
+            System.CodeDom.Compiler.CompilerResults res = prov.CompileAssemblyFromFile(cmpparam, Application.StartupPath + "\\AnalaizerClass.cs");
+            // Выводим результат компилирования на экран
+            if (res.Errors.Count != 0)
+            {
+                richTextBox1.Text += res.Errors[0].ToString();
+            }
+            else
+            {
+
+                // загружаем только что скомпилированную сборку(здесь тонкий момент - если мы прото загрузим сборку из файла - то он будет заблокирован,
+                // acces denied, поэтому вначале читаем его в поток, и лишь потом подключаем)
+                System.IO.BinaryReader reader = new System.IO.BinaryReader(new System.IO.FileStream(Application.StartupPath + "\\My.dll", System.IO.FileMode.Open, System.IO.FileAccess.Read));
+                Byte[] asmBytes = new Byte[reader.BaseStream.Length];
+                reader.Read(asmBytes, 0, (Int32) reader.BaseStream.Length);
+                reader.Close();
+                reader = null; 
+                System.Reflection.Assembly assm = System.Reflection.Assembly.Load(asmBytes);
+                Type[] types = assm.GetTypes();
+                Type analaizer = types[0];
+                // находим метод CheckCurrency - к счастью он единственный
+                System.Reflection.MethodInfo addinfo = analaizer.GetMethod("RunEstimate");
+                System.Reflection.FieldInfo fieldopz = analaizer.GetField("opz");
+                System.Collections.ArrayList ar = new System.Collections.ArrayList();
+                ar.Add("2");
+                ar.Add("2");
+                ar.Add("+");
+                fieldopz.SetValue(null, ar);
+                richTextBox1.Text += addinfo.Invoke(null, null).ToString();
+                asmBytes = null;
+            }
+            prov.Dispose();
+        }
+
+        private void buttonStartDel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = "";
+                richTextBox1.Text += "Test Case 1\n";
+                richTextBox1.Text += "Входные данные: a= 78508, b = -304\n";
+                richTextBox1.Text += "Ожидаемый результат: res = 76 && error = \"\""+"\n";
+                int res = CalcClass.Mod(78508, -304);
+                string error = CalcClass.lastError;
+                richTextBox1.Text += "Код ошибки: " + error + "\n";
+                richTextBox1.Text += "Получившийся результат: " +"res = "+ res.ToString() +" error = "+error.ToString() +"\n";
+                if (res == 76 && error == "")
+                {
+                    richTextBox1.Text += "Тест пройден\n\n";
+                }
+                else
+                {
+                    richTextBox1.Text += "Тест не пройден\n\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += "Перехвачено исключение: " + ex.ToString() + "\nТест не пройден.\n";
+            }
+
+            try
+            {
+                richTextBox1.Text += "Test Case 2\n";
+                richTextBox1.Text += "Входные данные: a= -2050800078, b = 3000000000\n";
+                richTextBox1.Text += "Ожидаемый результат: res = 0 && error = \"Error 06\"\n";
+                int res = CalcClass.Mod(-2050800078, 3000000000);
+                string error = CalcClass.lastError;
+                richTextBox1.Text += "Код ошибки: " + error + "\n";
+                richTextBox1.Text += "Получившийся результат: " + "res = " + res.ToString() + " error = " + error.ToString() + "\n";
+                if (res == 0 && error == "Error 06")
+                {
+                    richTextBox1.Text += "Тест пройден\n\n";
+                }
+                else
+                {
+                    richTextBox1.Text += "Тест не пройден\n\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += "Перехвачено исключение: " + ex.ToString() + "\nТест не пройден.\n";
+            }
+
+            try
+            {
+                richTextBox1.Text += "Test Case 3\n";
+                richTextBox1.Text += "Входные данные: a= 3000000000, b = -2050800078\n";
+                richTextBox1.Text += "Ожидаемый результат: res = 0 && error = \"Error 06\"\n";
+                int res = CalcClass.Mod(3000000000, -2050800078);
+                string error = CalcClass.lastError;
+                richTextBox1.Text += "Код ошибки: " + error+"\n";
+                richTextBox1.Text += "Получившийся результат: " + "res = " + res.ToString() + " error = " + error.ToString() + "\n";
+                if (res == 0 && error == "Error 06")
+                {
+                    richTextBox1.Text += "Тест пройден\n\n";
+                }
+                else
+                {
+                    richTextBox1.Text += "Тест не пройден\n\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += "Перехвачено исключение: " + ex.ToString() + "\nТест не пройден.\n";
+            }
+
+            try
+            {
+                richTextBox1.Text += "Test Case 4\n";
+                richTextBox1.Text += "Входные данные: a= 2000000000, b = 2000000000\n";
+                richTextBox1.Text += "Ожидаемый результат: res = 0 && error = \"\"\n";
+                int res = CalcClass.Mod(2000000000, 2000000000);
+                string error = CalcClass.lastError;
+                richTextBox1.Text += "Код ошибки: " + error +"\n";
+                richTextBox1.Text += "Получившийся результат: " + "res = " + res.ToString() + " error = " + error.ToString() + "\n";
+                if (res == 0 && error == "")
+                {
+                    richTextBox1.Text += "Тест пройден\n\n";
+                }
+                else
+                {
+                    richTextBox1.Text += "Тест не пройден\n\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += "Перехвачено исключение: " + ex.ToString() + "\nТест не пройден.\n";
+            }
+        }
+    }
+}
